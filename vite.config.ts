e05149b688eaa20f76541767fbde0f9fe6da5ed0
L1ght-time <light.time.1995@gmail.com>
2025-09-29 19:54:24 +0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/memcrab/",
   plugins: [react()],
-  base: "/my-memcrab/",
   server: {
     port: 3000,
   },
